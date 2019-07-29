e70a1948913186ec1b29b29ac5b058f01259e7b3
Yuqing <liu.yuqin@husky.neu.edu>
2019-07-28 22:33:53 -0400

    new

--- nbcproject_acting_positions/actingpositions.js
+++ nbcproject_acting_positions/actingpositions.js
@@ -1,5 +1,5 @@
 let small_screen = document.documentElement.clientWidth < 600 ? true : false
-var margin = { top: 30, left: 50, right: 60, bottom: 50},
+var margin = { top: 30, left: 50, right: 130, bottom: 50},
       height = 1300 - margin.top - margin.bottom,
       width = 1000 - margin.left - margin.right;
 console.log(margin)
